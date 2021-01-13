3db604a357acc336365280a5966bd52b9596d77e
Mike <alberto.miguel96@gmail.com>
2021-01-13 17:56:10 +0100

    Clase con tarea en segundo plano creada. Clase POST también, terminar el doInBackground para realizar la conexión entre servlet y android

--- Glovo_Android/app/src/main/java/com/example/glovo/listadoRestaurantes/modelo/ListadoRestaurantesModelo.java
+++ Glovo_Android/app/src/main/java/com/example/glovo/listadoRestaurantes/modelo/ListadoRestaurantesModelo.java
@@ -1,16 +1,55 @@
 package com.example.glovo.listadoRestaurantes.modelo;
 
+import android.os.AsyncTask;
+
+import com.example.glovo.beans.Restaurante;
 import com.example.glovo.listadoRestaurantes.interfaces.ListadoRestaurantesContrato;
+
+import java.util.ArrayList;
+
 
 public class ListadoRestaurantesModelo implements ListadoRestaurantesContrato.Modelo {
 
+    private ArrayList<Restaurante> listaRestaurantesArrayList;
+    private OnLstRestaurantesListener onLstRestaurantesListener;
 
-    public ListadoRestaurantesModelo() {
 
-    }
+    public ListadoRestaurantesModelo() { }
 
     @Override
     public void getRestaurantesWS(OnLstRestaurantesListener onLstRestaurantesListener) {
 
+        this.onLstRestaurantesListener = onLstRestaurantesListener;
+        TareaEnSegundoPlano hilo = new TareaEnSegundoPlano();
+        hilo.execute();
+
+//        onLstRestaurantesListener.onCorrecto();
+//        onLstRestaurantesListener.onError();
+
+
     }
+
+    /**
+     * Clase que realiza el asyntask en segundo plano a la base de datos y devuelve una respuesta al presenter con la programación reactiva
+     */
+    class TareaEnSegundoPlano extends AsyncTask<String, Integer, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean respuesta) {
+
+            if (respuesta) {
+                if (listaRestaurantesArrayList != null && listaRestaurantesArrayList.size() > 0) {
+                    onLstRestaurantesListener.onCorrecto(listaRestaurantesArrayList);
+                } else {
+                    onLstRestaurantesListener.onError("Error al traer los datos del servidor");
+                }
+            }
+        }
+    }
+
 }
